afe606a63ac8273e5a738e696d9327a85ed59872
SentientSam <samueltlamb1997@gmail.com>
2020-11-01 21:37:55 -0600

    Update to comments

--- Recursion.py
+++ Recursion.py
@@ -1,7 +1,7 @@
 # Author : Samuel Lamb
 # Date last worked on : 11/1/2020
 # Purpose : Reference example of recursion using a factorial function
-
+# Further use of recursion can be seen in the Quicksort.py file
 
 def factorial(x):
   if x == 1:
